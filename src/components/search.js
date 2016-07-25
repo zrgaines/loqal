21b3efe56e37ed8064971a63a72bcd5256467507
Colin Standefer <cstandefer@alumni.berklee.edu>
2016-07-25 13:28:06 -0500

    google maps returning coordinates from entered addresses

--- src/components/search.js
+++ src/components/search.js
@@ -5,14 +5,17 @@ class Search extends React.Component {
 
   constructor(props){
     super(props);
-
   }
-
+  _handleSearch(event) {
+    event.preventDefault();
+    this.props.search(this.refs.loqalSearch.value);
+  }
   render(){
     return(
-      <div>
-        Search
-      </div>
+      <form onSubmit={this._handleSearch.bind(this)}>
+        <input type="search" ref="loqalSearch" placeholder="Search for a city"/>
+        <input type="submit" value="Search"/>
+      </form>
     );
   }
 }
